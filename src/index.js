d4f823fdeeb63ff7a6a04d71c72e80a1a3d81abf
Edson_Mota <eedsonn@yahoo.com.br>
2020-03-28 21:57:53 -0300

    Backend

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Tipos de Parametros:
+
+// Query Params: Parametros nomeados enviados na rota apos ? (filtros, paginação)
+// Route Params: Parametros utilizados para identificar recursos
+// Request Body: Corpo da requisicao, utilizado para criar ou alterar recursos
+
+
+app.listen(3333);
